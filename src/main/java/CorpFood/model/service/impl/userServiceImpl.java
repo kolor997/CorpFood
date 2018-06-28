8b2c676c09675d6db24e9ec2d90ee65cac0d2460
kolor <kamil.konera1@gmail.com>
2018-06-28 19:25:49 +0200

    Remove idea added User

--- src/main/java/CorpFood/model/service/impl/userServiceImpl.java
+++ src/main/java/CorpFood/model/service/impl/userServiceImpl.java
@@ -1,12 +1,44 @@
 package CorpFood.model.service.impl;
 
 
+import CorpFood.model.dto.CreateUserDto;
+import CorpFood.model.entity.User;
 import CorpFood.model.repository.UserRepository;
+import CorpFood.model.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class userServiceImpl {
+import java.util.HashSet;
+import java.util.Set;
 
-//    private UserRepository
+@Service
+public class userServiceImpl implements userService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public userServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findOneById(Long id){return userRepository.getOne(id);}
+
+    @Override
+    public Set<User> findAll() {return new HashSet<>(userRepository.findAll());}
+
+    @Override
+    public void deleteUser(Long id) {userRepository.delete(id);}
+
+    @Override
+    public User createUser(CreateUserDto createUserDto) {
+        User user = new User();
+        user.setFirstName(createUserDto.getFirstName());
+        user.setLastName(createUserDto.getLastName());
+        user.setLogin(createUserDto.getLogin());
+        user.setPassword(createUserDto.getPassword());
+        return userRepository.save(user);
+    }
+
 
 }
