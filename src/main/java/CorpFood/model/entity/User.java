0397b2786ddf1bd58d9865df2040e9c3e5d34201
kolor <kamil.konera1@gmail.com>
2018-06-28 18:27:30 +0200

    Pom added Package added Application class added User Entity added and updated

--- src/main/java/CorpFood/model/entity/User.java
+++ src/main/java/CorpFood/model/entity/User.java
@@ -1,4 +1,57 @@
 package CorpFood.model.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String login;
+
+    protected User(){}
+
+    public User(String firstName, String lastName, String login) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
---
+++ src/main/java/CorpFood/model/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package CorpFood.model.repository;
+
+import CorpFood.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
