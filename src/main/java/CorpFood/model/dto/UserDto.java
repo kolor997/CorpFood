a99b835c3ec30b517ab754fda5656fec36304f9b
kolor <kamil.konera1@gmail.com>
2018-06-28 20:55:51 +0200

    Remove idea

--- src/main/java/CorpFood/model/dto/UserDto.java
+++ src/main/java/CorpFood/model/dto/UserDto.java
@@ -6,10 +6,14 @@ public class UserDto {
 
     private Long id;
     private String login;
+    private String firstName;
+    private String lastName;
 
     public UserDto(User user) {
         this.id = user.getId();
         this.login = user.getLogin();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
     }
 
     public Long getId() {
@@ -27,5 +31,21 @@ public class UserDto {
 
     public void setLogin(String login) {
         this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
