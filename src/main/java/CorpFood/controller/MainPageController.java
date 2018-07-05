cb7dcebadd67f2244b06644606e27648ff6207e2
kolor <kamil.konera1@gmail.com>
2018-07-05 18:30:25 +0200

    Remove idea

--- src/main/java/CorpFood/controller/MainPageController.java
+++ src/main/java/CorpFood/controller/MainPageController.java
@@ -1,15 +1,19 @@
 package CorpFood.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainPageController {
 
-    @GetMapping("/welcome")
-    public String pleas(ModelMap modelMap) {
-        modelMap.put("one", "Finally working!!!!!!");
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String foo(ModelMap model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        model.put("userName", username);
         return "FirstPage";
     }
 
