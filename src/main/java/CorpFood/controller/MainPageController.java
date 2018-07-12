3df19b73f88867e564eb0225a7de648e40eda16b
Daniel <daniel97111@wp.pl>
2018-07-12 18:35:23 +0200

    th:iteration - in first page

--- src/main/java/CorpFood/controller/MainPageController.java
+++ src/main/java/CorpFood/controller/MainPageController.java
@@ -8,6 +8,7 @@ import CorpFood.model.entity.UserResponse;
 import CorpFood.model.repository.UserRepository;
 import CorpFood.model.service.OfferService;
 import CorpFood.model.service.UserResponseService;
+import CorpFood.model.service.impl.ContentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Controller
@@ -27,14 +29,18 @@ public class MainPageController {
     private UserResponseService userResponseService;
     private OfferService offerService;
     private UserRepository userRepository; //userService
+    private ContentServiceImpl contentService;
 
 
     @Autowired
-    public MainPageController(UserResponseService userResponseService, OfferService offerService, UserRepository userRepository) {
+    public MainPageController(UserResponseService userResponseService, OfferService offerService, UserRepository userRepository, ContentServiceImpl contentService) {
         this.userResponseService = userResponseService;
         this.offerService = offerService;
         this.userRepository = userRepository;
+        this.contentService = contentService;
     }
+
+
 
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
     public ModelAndView showRegistrationPage(ModelAndView modelAndView, UserResponse userResponse) {
@@ -44,6 +50,11 @@ public class MainPageController {
 
         List<Offer> offers = offerService.findActiveOffers();
 
+
+
+        Map<OfferDTO, Set<UserResponse>> activeResponses =  contentService.getAllFoodOrder();
+
+        modelAndView.addObject("activeResponses",activeResponses);
         modelAndView.addObject("activeOffers", offers);
         modelAndView.addObject("responses", response);
 
@@ -59,6 +70,8 @@ public class MainPageController {
         UserDetails userPrincipals = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userRepository.findOneByLogin(userPrincipals.getUsername());
         modelAndView.addObject("userHeader", user.getFirstName() + " " + user.getLastName());
+
+
 
         Set<UserResponse> response = userResponseService.findAll();
         modelAndView.addObject("responses", response);
